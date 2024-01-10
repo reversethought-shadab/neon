263c8a1ba95978f0522954321a021ae29b00c1cc
reversethought-shadab <rt.developer.shadab@gmail.com>
2024-01-11 03:48:30 +0530

    function

---
+++ assets/components/header.js
@@ -0,0 +1,218 @@
+// Header Component
+class MyHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <header>
+    <section class="w-100">
+    <div class="container-fluid w-100 gx-0">
+
+      <div class="announcement-bar">
+        <p class="announcement-message text-center">
+          🎆 New Year 2024 Sale! Flat 24% OFF at Checkout
+        </p>
+      </div>
+    </div>
+  </section>
+  <div class="container-fluid topbar bg-dark d-none d-lg-block">
+    <div class="container px-0">
+        <div class="topbar-top d-flex justify-content-center  flex-lg-wrap">
+            <div class="top-info flex-grow-0 break">
+                <span class="rounded-circle btn-sm-square  me-2">
+                    <img src="assets/images/svg/warr.svg" alt="">
+                </span>
+                <div class=" border-white d-flex align-items-center">
+                    <p class="mb-0 text-white fs-6 fw-normal">2 Year Warranty</p>
+                </div>
+               
+            </div>
+            <div class="top-info flex-grow-0  break">
+                <span class="rounded-circle btn-sm-square  me-2">
+                    <img src="assets/images/svg/happy.svg" alt="">
+                </span>
+                <div class=" border-white d-flex align-items-center">
+                    <p class="mb-0 text-white fs-6 fw-normal">12000+ Happy Customers</p>
+                </div>
+               
+            </div>
+            <div class="top-info flex-grow-0 mx-3 break-2 ">
+                <span class="rounded-circle btn-sm-square  me-2">
+                    <img src="assets/images/svg/delivery.svg" alt="">
+                </span>
+                <div class="  border-white d-flex align-items-center">
+                    <p class="mb-0 text-white fs-6 fw-normal">100% Timely Delivery</p>
+                </div>
+               
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid bg-light">
+  <div class="container px-0">
+      <nav class="navbar navbar-light navbar-expand-xl">
+          <a href="index.html" class="navbar-brand">
+              <h1 class="text-primary display-4">Neon</h1>
+          </a>
+          <button class="navbar-toggler py-2 px-3 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-expanded="false">
+              <span class="fa fa-bars text-primary"></span>
+          </button>
+          <div class="navbar-collapse bg-light py-3 collapse" id="navbarCollapse" >
+              <div class="navbar-nav mx-auto border-top">
+                <div class="nav-item dropdown ">
+                    <a href="#" class="nav-link dropdown-toggle  active" data-bs-toggle="dropdown">Customize</a>
+                    <div class="dropdown-menu m-0  site-nav-dropdown ">
+                      <div class="site-nav__dropdown site-nav__dropdown--left custom_menu_list">
+                        <ul>
+                          <li>
+                            <a href="customize-neon.html" class="site-nav__link site-nav__child-link">
+                              <img class="menu_image_sub lazyloaded"
+                                data-src="assets/images/stories/1.jpg"
+                                loading="lazy"
+                                src="assets/images/stories/1.jpg">
+                              <span class="site-nav__label">Neon Sign<span class="menu_image_arrow_right"><i
+                                    class="fas fa-chevron-circle-right"></i></span>
+                            </a>
+                          </li>
+                          <li>
+                            <a href="customize-neon.html" class="site-nav__link site-nav__child-link">
+                              <img class="menu_image_sub lazyloaded"
+                                data-src="assets/images/stories/1.jpg"
+                                loading="lazy"
+                                src="assets/images/stories/1.jpg">
+                              <span class="site-nav__label">Neon Sign<span class="menu_image_arrow_right"><i
+                                    class="fas fa-chevron-circle-right"></i></span>
+                            </a>
+                          </li>
+                        
+                        </ul>
+                       
+                      
+                      </div>
+                    </div>
+                </div>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle  " data-bs-toggle="dropdown">Shop Neon</a>
+                    <div class="dropdown-menu m-0  site-nav-dropdown ">
+                      <div class="site-nav__dropdown site-nav__dropdown--left custom_menu_list">
+                        <ul>
+                          <li>
+                            <a href="customize-neon.html" class="site-nav__link site-nav__child-link">
+                              <img class="menu_image_sub lazyloaded"
+                                data-src="assets/images/stories/1.jpg"
+                                loading="lazy"
+                                src="assets/images/stories/1.jpg">
+                              <span class="site-nav__label">Neon Sign<span class="menu_image_arrow_right"><i
+                                    class="fas fa-chevron-circle-right"></i></span>
+                            </a>
+                          </li>
+                          <li>
+                            <a href="customize-neon.html" class="site-nav__link site-nav__child-link">
+                              <img class="menu_image_sub lazyloaded"
+                                data-src="assets/images/stories/1.jpg"
+                                loading="lazy"
+                                src="assets/images/stories/1.jpg">
+                              <span class="site-nav__label">Neon Sign<span class="menu_image_arrow_right"><i
+                                    class="fas fa-chevron-circle-right"></i></span>
+                            </a>
+                          </li>
+                          <li>
+                            <a href="customize-neon.html"
+                              class="site-nav__link site-nav__child-link site-nav__link--last">
+                              <img class="menu_image_sub lazyloaded"
+                                data-src="assets/images/stories/2.jpg"
+                                loading="lazy"
+                                src="assets/images/stories/2.jpg">
+                              <span class="site-nav__label">FloRo Sign <span class="menu_image_arrow_right"><i
+                                    class="fas fa-chevron-circle-right"></i></span>
+                            </a>
+                          </li>
+                        </ul>
+                        <ul>
+                          <li>
+                            <a href="customize-neon.html" class="site-nav__link site-nav__child-link">
+                              <img class="menu_image_sub lazyloaded"
+                                data-src="assets/images/stories/1.jpg"
+                                loading="lazy"
+                                src="assets/images/stories/1.jpg">
+                              <span class="site-nav__label">Neon Sign<span class="menu_image_arrow_right"><i
+                                    class="fas fa-chevron-circle-right"></i></span>
+                            </a>
+                          </li>
+                          <li>
+                            <a href="customize-neon.html" class="site-nav__link site-nav__child-link">
+                              <img class="menu_image_sub lazyloaded"
+                                data-src="assets/images/stories/1.jpg"
+                                loading="lazy"
+                                src="assets/images/stories/1.jpg">
+                              <span class="site-nav__label">Neon Sign<span class="menu_image_arrow_right"><i
+                                    class="fas fa-chevron-circle-right"></i></span>
+                            </a>
+                          </li>
+                          <li>
+                            <a href="customize-neon.html"
+                              class="site-nav__link site-nav__child-link site-nav__link--last">
+                              <img class="menu_image_sub lazyloaded"
+                                data-src="assets/images/stories/2.jpg"
+                                loading="lazy"
+                                src="assets/images/stories/2.jpg">
+                              <span class="site-nav__label">FloRo Sign <span class="menu_image_arrow_right"><i
+                                    class="fas fa-chevron-circle-right"></i></span>
+                            </a>
+                          </li>
+                        </ul>
+                      
+                      </div>
+                    </div>
+                </div>
+                 
+                  <a href="" class="nav-item nav-link">Head Light</a>
+                  <a href="" class="nav-item nav-link">Business Neon Logo</a>
+                  <a href="#" class="nav-item nav-link">Why Attack</a>
+              </div>
+              <div class="d-flex align-items-center flex-nowrap pt-xl-0">
+                  <button class="btn-search btn btn-primary btn-primary-outline-0  icon-circle" data-bs-toggle="modal" data-bs-target="#searchModal"><i class="fas fa-search "></i></button>
+                  <a href="#" class="btn btn-cart btn-primary-outline-0 icon-circle rounded-pill py-3 px-4 ms-4" id="cart-icon"><i class="fa-solid fa-cart-shopping"></i></a>
+                  <a href="login.html" class="btn btn-user btn-primary-outline-0 icon-circle rounded-pill py-3 px-4 ms-4"><i class="fa-solid fa-user"></i></a>
+                  <div class="cart">
+                    <h2 class="cart-title">Your Cart</h2>
+                    <div class="cart-content">
+                    </div>
+                    <!-- total -->
+                    <div class="total">
+                      <div class="total-title">Total</div>
+                      <div class="total-price">$0</div>
+                    </div>
+                    <!-- buy button -->
+                    <button class="btn-buy" type="button">Checkout</button>
+                    <!-- cart close -->
+                    <span id="close-cart">x</span>
+                    <!-- <i id="close-cart" class='fa fa-x'></i> -->
+                    <!-- <button id="close-cart" type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
+                  </div>
+              </div>
+          </div>
+      </nav>
+  </div>
+</div>
+
+<div class="modal " id="searchModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-fullscreen">
+      <div class="modal-content rounded-0">
+          <div class="modal-header">
+              <h4 class="modal-title mb-0" id="exampleModalLabel">Search by keyword</h4>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body d-flex align-items-center">
+              <div class="input-group w-75 mx-auto d-flex">
+                  <input type="search" class="form-control p-3" placeholder="keywords" aria-describedby="search-icon-1">
+                  <span id="search-icon-1" class="input-group-text p-3"><i class="fa fa-search"></i></span>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
+</header>
+ `;
+  }
+}
+
+customElements.define("my-header", MyHeader);
---
+++ assets/js/main.js
@@ -0,0 +1,216 @@
+// Fixed Navbar
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 300) {
+    $(".sticky-top").addClass("shadow-sm").css("top", "0px");
+  } else {
+    $(".sticky-top").removeClass("shadow-sm").css("top", "-200px");
+  }
+});
+//  custom cursor
+const cursor = document.querySelector("#cursor");
+const cursorBorder = document.querySelector("#cursor-border");
+const cursorPos = {
+  x: 0,
+  y: 0,
+};
+const cursorBorderPos = {
+  x: 0,
+  y: 0,
+};
+
+document.addEventListener("mousemove", (e) => {
+  cursorPos.x = e.clientX;
+  cursorPos.y = e.clientY;
+
+  cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+});
+
+requestAnimationFrame(function loop() {
+  const easting = 8;
+  cursorBorderPos.x += (cursorPos.x - cursorBorderPos.x) / easting;
+  cursorBorderPos.y += (cursorPos.y - cursorBorderPos.y) / easting;
+
+  cursorBorder.style.transform = `translate(${cursorBorderPos.x}px, ${cursorBorderPos.y}px)`;
+  requestAnimationFrame(loop);
+});
+
+document.querySelectorAll("[data-cursor]").forEach((item) => {
+  item.addEventListener("mouseover", (e) => {
+    if (item.dataset.cursor === "pointer") {
+      cursorBorder.style.backgroundColor = "rgba(0, 144, 144, .4)";
+      cursorBorder.style.setProperty("--size", "55px");
+    }
+    if (item.dataset.cursor === "pointer2") {
+      cursorBorder.style.backgroundColor = "white";
+      cursorBorder.style.mixBlendMode = "difference";
+      cursorBorder.style.setProperty("--size", "60px");
+    }
+  });
+  item.addEventListener("mouseout", (e) => {
+    cursorBorder.style.backgroundColor = "unset";
+    cursorBorder.style.mixBlendMode = "unset";
+    cursorBorder.style.setProperty("--size", "30px");
+  });
+});
+function loginClick() {
+  var loginTab = document.getElementById("login-tab");
+  loginTab.click();
+}
+function signupClick() {
+  var signupTab = document.getElementById("signup-tab");
+  signupTab.click();
+}
+// back to top
+const tapTopBtn = document.querySelector(".tap-to-top-button");
+const tapTopTopBox = document.querySelector(".tap-to-top-box");
+tapTopBtn?.addEventListener("click", function () {
+  window.scroll({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+if (tapTopTopBox) {
+  window.onscroll = function () {
+    if (pageYOffset >= 1000) {
+      tapTopTopBox.classList.remove("hide");
+    } else {
+      tapTopTopBox.classList.add("hide");
+    }
+  };
+}
+//  cart
+// ==== CART ==================
+let cartIcon = document.querySelector("#cart-icon");
+let cart = document.querySelector(".cart");
+let closeCart = document.querySelector("#close-cart");
+
+// == Open cart
+cartIcon.onclick = () => {
+  cart.classList.add("active");
+};
+
+// == Close cart
+closeCart.onclick = () => {
+  cart.classList.remove("active");
+};
+
+// == Close Working JavaScript
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
+
+// == Making Function
+function ready() {
+  // Remove items from cart
+  var removeCartButtons = document.getElementsByClassName("cart-remove");
+  //   console.log(removeCartButtons);
+  for (var i = 0; i < removeCartButtons.length; i++) {
+    var button = removeCartButtons[i];
+    button.addEventListener("click", removeCartItem);
+  }
+  // == Quantity changes
+  var quantityInputs = document.getElementsByClassName("cart-quantity");
+  for (var i = 0; i < quantityInputs.length; i++) {
+    var input = quantityInputs[i];
+    input.addEventListener("change", quantityChanged);
+  }
+  // Add to cart
+  var addCart = document.getElementsByClassName("add-cart");
+  for (var i = 0; i < addCart.length; i++) {
+    var button = addCart[i];
+    button.addEventListener("click", addCartClicked);
+  }
+
+  // Buy button work
+  document
+    .getElementsByClassName("btn-buy")[0]
+    .addEventListener("click", buyButtonClicked);
+}
+
+// Buy button
+function buyButtonClicked() {
+  alert("Your order is placed");
+  var cartContent = document.getElementsByClassName("cart-content")[0];
+  while (cartContent.hasChildNodes()) {
+    cartContent.removeChild(cartContent.firstChild);
+  }
+  updatetotal();
+}
+
+// == Remove items from cart
+function removeCartItem(event) {
+  var buttonClicked = event.target;
+  buttonClicked.parentElement.remove();
+  updatetotal();
+}
+
+// == Quantity changes
+function quantityChanged(event) {
+  var input = event.target;
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
+  }
+  updatetotal();
+}
+
+// Add to cart
+function addCartClicked(event) {
+  var button = event.target;
+  var shopProducts = button.parentElement;
+  var title = shopProducts.getElementsByClassName("product-title")[0].innerText;
+  var price = shopProducts.getElementsByClassName("price")[0].innerText;
+  var productImg = shopProducts.getElementsByClassName("product-img")[0].src;
+  addProductToCart(title, price, productImg);
+  updatetotal();
+}
+function addProductToCart(title, price, productImg) {
+  var cartShopBox = document.createElement("div");
+  cartShopBox.classList.add("cart-box");
+  var cartItems = document.getElementsByClassName("cart-content")[0];
+  var cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
+  for (var i = 0; i < cartItemsNames.length; i++) {
+    if (cartItemsNames[i].innerText == title) {
+      alert("You have already add this item to cart");
+      return;
+    }
+  }
+
+  var cartBoxContent = `
+  <img src="${productImg}" alt="image" class="cart-img">
+		<div class="detail-box">
+			<div class="cart-product-title">${title}</div>
+			<div class="cart-price">${price}</div>
+			<input class="cart-quantity" type="number" value="1">
+		</div>
+	<i class='bx bxs-trash-alt cart-remove'></i>
+`;
+
+  cartShopBox.innerHTML = cartBoxContent;
+  cartItems.append(cartShopBox);
+  cartShopBox
+    .getElementsByClassName("cart-remove")[0]
+    .addEventListener("click", removeCartItem);
+  cartShopBox
+    .getElementsByClassName("cart-quantity")[0]
+    .addEventListener("change", quantityChanged);
+}
+// == Uptade total
+function updatetotal() {
+  var cartContent = document.getElementsByClassName("cart-content")[0];
+  var cartBoxes = cartContent.getElementsByClassName("cart-box");
+  var total = 0;
+  for (var i = 0; i < cartBoxes.length; i++) {
+    var cartBox = cartBoxes[i];
+    var priceElement = cartBox.getElementsByClassName("cart-price")[0];
+    var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
+    var price = parseFloat(priceElement.innerText.replace("$", ""));
+    var quantity = quantityElement.value;
+    total = total + price * quantity;
+  }
+  // if price contain some cents value
+  total = Math.round(total * 100) / 100;
+
+  document.getElementsByClassName("total-price")[0].innerText = "$" + total;
+}
